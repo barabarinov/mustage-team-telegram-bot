9057f0fe695b2b3fb038895289324735733164eb
barabarinov <barabarinov@gmail.com>
2024-05-14 19:30:50 +0300

    Added constants

---
+++ app/constants.py
@@ -0,0 +1,10 @@
+URL = "https://www.google.com/finance/quote/USD-UAH"
+DB_PATH = "exchange_rate.db"
+XLSX_FILE_PATH = "exchange_rate.xlsx"
+GREETING_TEXT = (
+    "Hey there {}! 🤙🏼🖖🏼"
+    "\nI can send you updates on how USD / UAH exchange rate "
+    "is changing throughout the day. "
+    "Just click the button below "
+    "if you'd like to get these reports every hour! 👇🏼"
+)
