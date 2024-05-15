718d9027d87c9bf69b6a1c2ac5559c74a7043c1d
barabarinov <barabarinov@gmail.com>
2024-05-15 15:12:39 +0300

    Added custom exception

--- app/parser.py
+++ app/parser.py
@@ -5,12 +5,24 @@ from app.constants import URL
 from app.handlers.utils import DBHandler
 
 
+class InvalidHtml(Exception):
+    pass
+
+
 def fetch_exchange_rate() -> float:
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    text = soup.find("div", class_="fxKbKc").text.strip()
+    div_with_data_entity_type_3 = soup.find("div", attrs={"data-entity-type": "3"})
 
+    if not div_with_data_entity_type_3:
+        raise InvalidHtml("The attribute 'data-entity-type=3' is missing!")
+
+    sixth_div = div_with_data_entity_type_3.find_all("div")[5]
+    if not sixth_div:
+        raise InvalidHtml("The HTML structure has changed!")
+
+    text = sixth_div.text.strip()
     return float(text.replace(",", "."))
 
 
