f7ff999a5bc205c0274736dc08e079d0a085a8a6
barabarinov <barabarinov@gmail.com>
2024-05-18 12:10:39 +0300

    Added run_job_hourly task to start function

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,10 +1,26 @@
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup
+from telegram import Update
 from telegram.ext import ContextTypes
 
 from app.constants import GREETING_TEXT
+from app.parser import update_exchange_rate_hourly
+
+
+async def callback_hour(context: ContextTypes.DEFAULT_TYPE) -> None:
+    update_exchange_rate_hourly()
+
+
+async def run_job_hourly(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE,
+) -> None:
+    context.job_queue.run_repeating(callback_hour, interval=3600, first=1)
+    print("✅ Job started successfully!")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await run_job_hourly(update=update, context=context)
+
     reply_markup = ReplyKeyboardMarkup([["/get_exchange_rate"]], resize_keyboard=True)
 
     await update.effective_message.reply_text(
